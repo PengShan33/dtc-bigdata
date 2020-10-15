24213667e14531922c26d528582b377dcd9d0356
Supertra <1427787077@qq.com>
2020-10-15 14:54:52 +0800

    中兴交换机。

--- dtc-flink-snmp/src/main/java/com/dtc/java/analytic/V2/process/function/ZXSwitchProcessMapFunction.java
+++ dtc-flink-snmp/src/main/java/com/dtc/java/analytic/V2/process/function/ZXSwitchProcessMapFunction.java
@@ -27,7 +27,7 @@ public class ZXSwitchProcessMapFunction extends ProcessWindowFunction<DataStruct
         for (DataStruct in : iterable) {
             String code = in.getZbFourName();
             //判断是否是数据
-            boolean strResult = in.getValue().matches("(^-?[0-9][0-9]*(.[0-9]+)?)$");
+            boolean strResult = in.getValue().matches("-?[0-9]+.*[0-9]*");
             if (!strResult) {
                 log.info("Value is not number of string!");
             } else {
@@ -44,58 +44,6 @@ public class ZXSwitchProcessMapFunction extends ProcessWindowFunction<DataStruct
                      *其中ZB_code为例：机器ip,1.0表示机框1，0板卡，表示机框1，板卡0的cpu使用率
                      */
                     collector.collect(new DataStruct(in.getSystem_name(), in.getHost(), in.getZbFourName(), in.getZbLastCode(),in.getNameCN(), in.getNameEN(), in.getTime(),in.getValue()));
-                }
-
-                if (code.equals("102_103_103_109_109")) {
-                    String new_value = in.getValue() + "_" + in.getTime();
-                    if (mapSwitch.containsKey(in.getHost() + "." + in.getZbFourName())) {
-                        String[] s = mapSwitch.get(in.getHost() + "." + in.getZbFourName()).split("_");
-                        double lastValue = Double.parseDouble(s[0]);
-                        long lastTime = Long.parseLong(s[1]);
-                        double currentValue = Double.parseDouble(in.getValue());
-                        long currentTime = Long.parseLong(in.getTime());
-                        double result = 0;
-                        try {
-                            /**
-                             * 交换机返回的为累加统计，所以需要中间等待后，再次获取，计算两次的差
-                             *  返回的值为byte，需要先乘8，计算为bit然后再除1024，计算进位量
-                             */
-                            result = 8 * Math.abs((lastValue - currentValue)) / 1024 / ((currentTime - lastTime) / 1000);
-                        } catch (ArithmeticException exc) {
-                            log.error("交换机端口出速率计算时，时间差为0.", exc);
-                        }
-                        collector.collect(new DataStruct(in.getSystem_name(), in.getHost(), "102_101_103_104_105", in.getZbLastCode(), in.getNameCN(), in.getNameEN(), in.getTime(), String.valueOf(result)));
-                        mapSwitch.put(in.getHost() + "." + in.getZbFourName(), new_value);
-                        continue;
-                    } else {
-                        mapSwitch.put(in.getHost() + "." + in.getZbFourName(), new_value);
-                        continue;
-                    }
-                }
-                if (code.equals("102_103_103_108_108")) {
-                    String new_value = in.getValue() + "_" + in.getTime();
-                    if (mapSwitch.containsKey(in.getHost() + "." + in.getZbFourName())) {
-                        String[] s = mapSwitch.get(in.getHost() + "." + in.getZbFourName()).split("_");
-                        double lastValue = Double.parseDouble(s[0]);
-                        long lastTime = Long.parseLong(s[1]);
-                        double currentValue = Double.parseDouble(in.getValue());
-                        long currentTime = Long.parseLong(in.getTime());
-                        double result;
-                        try {
-                            /**
-                             * 交换机返回的为累加统计，所以需要中间等待后，再次获取，计算两次的差
-                             *  返回的值为byte，需要先乘8，计算为bit然后再除1024，计算进位量
-                             */
-                            result = 8 * Math.abs((lastValue - currentValue)) / 1024 / ((currentTime - lastTime) / 1000);
-                        } catch (ArithmeticException exc) {
-                            log.error("交换机端口入速率计算时，时间差为0.", exc);
-                            continue;
-                        }
-                        collector.collect(new DataStruct(in.getSystem_name(), in.getHost(), "102_101_103_105_106", in.getZbLastCode(), in.getNameCN(), in.getNameEN(), in.getTime(), String.valueOf(result)));
-                        mapSwitch.put(in.getHost() + "." + in.getZbFourName(), new_value);
-                    } else {
-                        mapSwitch.put(in.getHost() + "." + in.getZbFourName(), new_value);
-                    }
                 }
             }
         }
